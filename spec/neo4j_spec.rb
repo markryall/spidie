bfaeb14ce17f9fe707abe8ea6693c841acb08c1e
duana <strongcoffee@gmail.com>
2010-10-02 21:42:17 +0800

    stores a url node with neo4j

---
+++ spec/neo4j_spec.rb
@@ -0,0 +1,21 @@
+require 'neo4jr-simple'
+
+Neo4jr::Configuration.database_path = File.join(File.expand_path(File.dirname(__FILE__)), 'test-spider-database')
+
+describe "spider database" do
+  it 'should store a page by url' do
+    url = 'http://link'
+    
+    Neo4jr::DB.execute do |neo|
+      node = neo.createNode
+      node[:identifier] = url
+    end
+
+    found_node = Neo4jr::DB.execute do |neo|
+      neo.find_node_by_identifier(url)
+    end
+    
+    found_node[:identifier].should == url
+
+  end
+end
