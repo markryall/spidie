9ead127a7ae8c0d64b51f762a49c53f657af08d5
duana <strongcoffee@gmail.com>
2010-10-03 00:03:27 +1000

    start driving out the page store api

--- spec/neo4j_spec.rb
+++ spec/neo4j_spec.rb
@@ -5,18 +5,28 @@ Neo4jr::Configuration.database_path = File.join(File.expand_path(File.dirname(__
 
 describe "spider database" do
   it 'should store a page by url' do
+    
+    page = Page.new(url)
+    Spidie::Store.put(page)
+    
+    found_page = Spidie::Store.retrieve(url)
+    found_page.url.should == url
+    
+=begin
     url = 'http://link'
-    
-    Neo4jr::DB.execute do |neo|
-      node = neo.createNode
-      node[:identifier] = url
-    end
+       
+       Neo4jr::DB.execute do |neo|
+         node = neo.createNode
+         node[:identifier] = url
+       end
 
-    found_node = Neo4jr::DB.execute do |neo|
-      neo.find_node_by_identifier(url)
-    end
-    
-    found_node[:identifier].should == url
+       found_node = Neo4jr::DB.execute do |neo|
+         neo.find_node_by_identifier(url)
+       end
+       
+       found_node[:identifier].should == url
+=end
+   
 
   end
 end
