b8c2fbf0c04da15bdf200fb9e354ade9a369be13
Mark Ryall <mark@ryall.name>
2010-10-02 22:32:10 +1000

    added link extractor for maximum link extraction and changed job to use it (doesn't create new jobs yet - that would just be insane)

--- lib/spidie/job.rb
+++ lib/spidie/job.rb
@@ -1,9 +1,16 @@
+require 'resque'
+require 'spidie/link_extractor'
+
 module Spidie
   module Job
     @queue = :urls
     
     def self.perform url
       puts "doing job hardcore with url #{url}"
+      LinkExtractor.new(url).each do |link|
+        puts link
+        #Resque.enqueue Spidie::Job, url
+      end
     end
   end
 end
