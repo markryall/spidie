74f1e54edf1595d2d4843495564367aa22260072
Mark Ryall <mark@ryall.name>
2010-10-11 22:54:14 +1000

    added script to dump the contents of the database

---
+++ lib/spidie/dumper.rb
@@ -0,0 +1,27 @@
+require 'neo4j'
+require 'spidie/store'
+
+module Spidie
+  class Dumper
+    def dump
+      Neo4j::Transaction.run do
+        Neo4j.info
+        puts
+        Neo4j.all_nodes do |node|
+          display_node node
+          node.rels.each do |rel|
+            puts "  #{rel.relationship_type}"
+            display_node rel.end_node, 1
+          end
+        end
+      end
+    end
+  private
+    def display_node node, indent=0
+      puts "#{'  '*indent}Node id #{node.neo_id}"
+      node.props.keys.each do |key|
+        puts "#{'  '*indent}  #{key}=#{node.props[key]}"
+      end
+    end
+  end
+end
