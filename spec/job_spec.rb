0ed67d1f59e5bff4859eeb9cfec522680aa831da
Mark Ryall <mark@ryall.name>
2010-10-12 08:42:29 +1000

    fixed job spec and marked empty example as pending

--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -1,20 +1,17 @@
 require File.dirname(__FILE__)+'/spec_helper'
 describe "job" do
   it "should fetch a page, store it and enqueue the links" do
-    pending
-    url, link1, link2= "url",  "http://link1",  "http://link2"
-    
+    url, url1, url2= "url",  "http://link1",  "http://link2"
+    link1, link2 = stub('link1', :url => url1), stub('link2', :url => url2)
     page = stub('page', :links => [link1, link2])
     Page.should_receive(:retrieve).with(url).and_return page
     page.should_receive(:store)
     
-    Resque.should_receive(:enqueue).with(Job, link1)
-    Resque.should_receive(:enqueue).with(Job, link2)
+    Resque.should_receive(:enqueue).with(Job, url1)
+    Resque.should_receive(:enqueue).with(Job, url2)
     
     Job.perform url
   end
 
-  it "should avoid cycles or spider will get dizzy" do
-    
-  end
+  it "should avoid cycles or spider will get dizzy"
 end
