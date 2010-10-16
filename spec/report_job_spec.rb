1333e86c2a7b619d6898fc8e5811623957bed4cc
duana <strongcoffee@gmail.com>
2010-10-16 21:26:19 +1000

    finished spec for ReportJob

--- spec/report_job_spec.rb
+++ spec/report_job_spec.rb
@@ -2,12 +2,14 @@ require File.dirname(__FILE__)+'/spec_helper'
 
 require 'json'
 require 'spidie/report'
+require 'spidie/report_job'
 
 describe "report job" do
   include Store
 
   before do
     clean_db
+    FileUtils.rm "report", :force => true
   end
 
   it "should report stuff" do
@@ -16,17 +18,12 @@ describe "report job" do
       Page.new :url => "broken_page", :broken => true
     end
     
-    report_file = "report"
-
-    while_shopping do
-      report = Report.new
-      report.total_pages = pages.count
-      report.num_broken = broken_pages.count
+    ReportJob.should_receive(:pages).and_return double('pages', :count => 2)
+    ReportJob.should_receive(:broken_pages).and_return double('broken_pages', :count => 1)
+     
+    ReportJob.perform
       
-      open(report_file, 'w') {|f| f.puts report.to_json }
-    end
-      
-    report_json = JSON.parse open(report_file).read
+    report_json = JSON.parse open("report").read
     report_json["total_pages"].should == 2
     report_json["num_broken"].should == 1
   end
