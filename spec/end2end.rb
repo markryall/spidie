c5dc6ce68d3f0b1ea7c3b30d40197f4d0a5345eb
duana <strongcoffee@gmail.com>
2010-10-17 17:53:11 +1000

    rejigged the end2end tests to be a bit more useful.  main scenario is not working tho even tho the pages look correct in the database. hmmm...

--- spec/end2end.rb
+++ spec/end2end.rb
@@ -26,25 +26,28 @@ describe 'the spider, the spider' do
     wait_for_file SUCCESS_FILE
   end
   
-  it 'should consume with eagerness the url for a page with no links' do
-    Resque.enqueue Spidie::Job, 'http://localhost:4567/0_index.html'
-    verify_url 'http://localhost:4567/0_index.html'
+  it 'should embark on a tasty mission with all sorts of links' do
+    Resque.enqueue Spidie::Job, 'http://localhost:4567/page_with_two_working_links_and_one_broken.html'
+
+    Resque.enqueue Spidie::ReportJob
+    wait_for_file REPORT_FILE
+    
+    working_urls = ["http://localhost:4567/page_with_two_working_links_and_one_broken.html",
+     "http://localhost:4567/page_with_no_links.html",
+     "http://localhost:4567/page_with_relative_links_one_fine_one_broken.html",
+     "http://localhost:4567/working_relative_link.html"]
+    broken_urls = ["http://localhost:4567/broken_link.html","http://localhost:4567/broken_relative_link.html"]
+    
+    report = JSON.parse open(REPORT_FILE).read
+    report["total_pages"].should == 6
+    report["num_broken"].should == 2
+    working_urls.each {|url| report["good_pages"].should include url}
+    broken_urls.each {|url| report["broken_pages"].should include url}
   end
 
-  it 'should consume with gusto the url for a page with a single absolute link' do
-    Resque.enqueue Spidie::Job, 'http://localhost:4567/1_index.html'
-    verify_url 'http://localhost:4567/1_index.html'
-    verify_url 'http://localhost:4567/1_link.html'
-  end
-
-  it 'should consume with relish the url for a page with a single relative link' do
-    Resque.enqueue Spidie::Job, 'http://localhost:4567/2_index.html'
-    verify_url 'http://localhost:4567/2_index.html'
-    verify_url 'http://localhost:4567/2_link.html'
-  end
   
-  it 'should consume with eagerness the url for a page with no links and give a report' do
-    url = 'http://localhost:4567/hi.html'
+  it 'should consume without disappointment a single broken link' do
+    url = 'http://localhost:4567/doesnt_exist.html'
     Resque.enqueue Spidie::Job, url
 
     Resque.enqueue Spidie::ReportJob
--- spec/test_application.rb
+++ spec/test_application.rb
@@ -4,36 +4,36 @@ require 'markaby'
 
 File.open('tmp/test_web.pid', 'w') {|f| f.puts Process.pid }
 
-get '/0_index.html' do
-  ''
-end
-
-get '/1_index.html' do
+get '/page_with_two_working_links_and_one_broken.html' do
   Markaby::Builder.new.html do
     head { title 'index.html' }
     body do
       ul {
-        li { a 'a link', :href => 'http://localhost:4567/1_link.html' }
+        li { a 'a link', :href => 'http://localhost:4567/page_with_relative_links_one_fine_one_broken.html' }
+        li { a 'a link', :href => 'http://localhost:4567/page_with_no_links.html' }
+        li { a 'a link', :href => 'http://localhost:4567/broken_link.html' }
       }
     end
   end
 end
 
-get '/1_link.html' do
+get '/page_with_no_links.html' do
   ''
 end
 
-get '/2_index.html' do
+get '/page_with_relative_links_one_fine_one_broken.html' do
   Markaby::Builder.new.html do
     head { title 'index.html' }
     body do
       ul {
-        li { a 'a link', :href => '/2_link.html' }
+        li { a 'a link', :href => '/working_relative_link.html' }
+        li { a 'a link', :href => '/broken_relative_link.html' }
       }
     end
   end
 end
 
-get '/2_link.html' do
+get '/working_relative_link.html' do
   ''
 end
+
